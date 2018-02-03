8bdd00b8ccc9c0f9b86b43d92f23be897e8ecdde
AvantikaYellapantula <av.yellapantula.cs@gmail.com>
2018-02-03 13:19:13 -0500

    controller,main,fxml_layout

---
+++ SongLib.java
@@ -0,0 +1,34 @@
+package application;
+	
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SongLib extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("songLib_layout.fxml"));
+        primaryStage.setTitle("Song Library");
+        
+    
+        
+    
+    
+    
+    
+    
+        primaryStage.setScene(new Scene(root, 685, 688));
+        primaryStage.show();
+    
+    }
+
+
+}
